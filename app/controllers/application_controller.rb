212515a4d4a05596d4b5b8516fc587d7307be212
Stephen Blais <stephen@s-blais.com>
2019-11-04 23:57:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 
 require_relative '../../config/environment'
 
+# For some reason, it is impossible to exit shotgun after launching it. To be able to do anything else, I have to quite LearnIDE then reopen
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -9,5 +11,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to '/articles'
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    # I could not do the below as just @article.update(params) without getting an error because it was sending "_method" as part of the params hash, isn't it supposed to know better than to do that?
+    @article.update(:title => params[:title], :content => params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end
+
 end
